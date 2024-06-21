2aea43bfa5c9aed8a8af9e25b906b27408264813
ebo_shit <isakovn2005@gmail.com>
2024-06-21 04:57:08 +0300

    m

--- service/OpenAI/Base.py
+++ service/OpenAI/Base.py
@@ -1,72 +1,100 @@
 import logging
 import time
 
-import openai
-from aiogram.enums import ChatAction
-from openai.pagination import SyncCursorPage
-from openai.types import FileObject
-from openai.types.beta import Thread
-from openai.types.beta.threads import Run, ThreadMessage
+from openai import OpenAI
+from openai.types.beta import CodeInterpreterToolParam
+from openai.types.beta.threads.message_create_params import Attachment
 
-from data.config import OPENAI_API_KEY, ASSISTANT, bot
+from data.config import OPENAI_API_KEY, ASSISTANT
 
 logger = logging.getLogger(__name__)
 
 
-class BaseOpenAI:
+class BaseClient:
     def __init__(self):
-        self.client = openai
+        self.client = OpenAI()
         self.client.api_key = OPENAI_API_KEY
 
-    def _request(self, thread_id: str, content: str) -> Run:
-        self.client.beta.threads.messages.create(
-            thread_id=thread_id,
-            role="user",
-            content=content
-        )
-        run = self.client.beta.threads.runs.create(
-            thread_id=thread_id,
-            assistant_id=ASSISTANT,
-        )
-        return run
-
-    @staticmethod
-    def _get_text(messages, run_id) -> str:
-        text = ""
-        for message in messages:
-            if message.assistant_id is None:
-                continue
-            if message.run_id == run_id:
-                text = f"{message.content[0].text.value}"
-        return text
-
-    async def _wait_on_run(self, run, thread, user_id: int = None) -> Run:
-        while run.status == "queued" or run.status == "in_progress":
-            run = self.client.beta.threads.runs.retrieve(
-                thread_id=thread.id,
-                run_id=run.id,
-            )
-            if user_id:
-                await bot.send_chat_action(chat_id=user_id, action=ChatAction.TYPING, request_timeout=1.5)
-            time.sleep(1.5)
-        return run
-
-    def _get_response(self, thread) -> SyncCursorPage[ThreadMessage]:
-        return self.client.beta.threads.messages.list(thread_id=thread.id, order="asc")
-
-    def _new_threads(self) -> Thread:
+    def _create_thread(self):
         thread = self.client.beta.threads.create()
-        logger.info(f"Created new threads: {thread}")
         return thread
 
-    def _load_file(self, file) -> FileObject:
+    def _retrieve_thread(self, thread_id):
+        thread = self.client.beta.threads.retrieve(thread_id)
+        return thread
+
+    def _create_message(self, thread_id, content, file_id=None):
+        if file_id:
+            attachments = [Attachment(file_id=file_id, tools=[CodeInterpreterToolParam(type="code_interpreter")])]
+        else:
+            attachments = None
+        message = self.client.beta.threads.messages.create(
+            thread_id=thread_id,
+            role="user",
+            content=content,
+            attachments=attachments
+        )
+        logger.info(f"Created message: {message.id}")
+        return message
+
+    def _list_message(self, thread_id):
+        messages = self.client.beta.threads.messages.list(thread_id=thread_id,
+                                                          order="desc")
+        logging.info("Listing messages")
+        return messages
+
+    def _retrieve_message(self, thread_id, message_id):
+        message = self.client.beta.threads.messages.retrieve(
+            message_id=thread_id,
+            thread_id=message_id,
+        )
+        return message
+
+    def _create_run(self, thread_id):
+        run = self.client.beta.threads.runs.create(
+            thread_id=thread_id,
+            assistant_id=ASSISTANT
+        )
+        logger.info(f"Created run: {run.id}")
+        self._retrieve_run(run, thread_id)
+        return run
+
+    def _retrieve_run(self, run, thread_id):
+        while run.status != "completed":
+            run = self.client.beta.threads.runs.retrieve(
+                thread_id=thread_id,
+                run_id=run.id
+            )
+            logger.info(f"Retrieved run: {run.id}, run status: {run.status}")
+            time.sleep(2)
+
+        return run
+
+    def _upload_file(self, file):
         file = self.client.files.create(
             file=file,
             purpose="assistants")
+        logger.info(f"Uploaded file: {file.id}")
         return file
 
-    def _add_file(self, id) -> None:
-        self.client.beta.assistants.update(
-            ASSISTANT,
-            tools=[{"type": "code_interpreter"}, {"type": "retrieval"}],
-            file_ids=[id])
+    def _list_file(self):
+        files = self.client.files.list()
+        return files
+
+    def _delete_file(self, file_id):
+        self.client.files.delete(file_id)
+
+    @staticmethod
+    def _get_text(messages, run_id):
+        text = ""
+        for message in messages:
+            if message.run_id == run_id:
+                text = message.content[0].text.value
+        return text
+
+    def _del_copy_file(self, file_path):
+        list_files = self._list_file()
+        file_name = file_path.name.split("/")[-1]
+        for i in list_files:
+            if i.filename == file_name:
+                self._delete_file(i.id)
--- service/OpenAI/ChatGPT.py
+++ service/OpenAI/ChatGPT.py
@@ -1,25 +1,28 @@
-import logging
-
-from aiogram.enums import ChatAction
-
-from data.config import bot
-from service.OpenAI.Base import BaseOpenAI
-
-logger = logging.getLogger(__name__)
+from service.OpenAI.Base import BaseClient
 
 
-class ClientOpenAI(BaseOpenAI):
+class Client(BaseClient):
+    def analysis(self, file_path, thread_id=None):
+        if not thread_id:
+            thread = self._create_thread()
+            thread_id = thread.id
+        with open(file_path, "rb") as f:
+            file = self._upload_file(f)
+            self._create_message(thread_id=thread_id,
+                                 content=f"Проанализируй документ. В начале пиши название файла",
+                                 file_id=file.id)
+        return self.__answer(thread_id), thread_id
 
-    async def get_answer(self, user_id, content: str, file=None, thread_id=None) -> tuple[str, str]:
-        await bot.send_chat_action(chat_id=user_id, action=ChatAction.TYPING)
-        if thread_id is None:
-            thread = self._new_threads()
-        else:
-            thread = self.client.beta.threads.retrieve(thread_id=thread_id)
-            logger.info(f"Getting threads: {thread}")
-        if file is not None:
-            file_load = self._load_file(file)
-            self._add_file(file_load.id)
-        run = self._request(thread_id=thread.id, content=content)
-        await self._wait_on_run(run, thread, user_id)
-        return self._get_text(self._get_response(thread), run.idgit), thread.id
+    def question(self, content, thread_id=None):
+        if not thread_id:
+            thread = self._create_thread()
+            thread_id = thread.id
+        self._create_message(thread_id=thread_id,
+                             content=content)
+        return self.__answer(thread_id), thread_id
+
+    def __answer(self, thread_id):
+        run = self._create_run(thread_id)
+        messages = self._list_message(thread_id)
+        text = self._get_text(messages, run.id)
+        return text
